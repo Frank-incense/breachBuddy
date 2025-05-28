b72e4c45e3a9fa86afdc467fc6e210930a53e440
Frankincense <frankincensewelsey@gmail.com>
2025-05-28 12:56:43 +0300

    [Feature]: Added a password check function that that calls an API and checks the number of times a hash prefix occurs

--- lib/db/models.py
+++ lib/db/models.py
@@ -11,11 +11,11 @@ class User(Base):
     username = Column(String(), nullable=False)
     createdAt = Column(DateTime(), server_default=func.now())
 
-    # emails = relationship("EmailCheck", backref=backref("user"))
+    emails = relationship("EmailCheck", backref=backref("user"))
     passwords = relationship("PasswordCheck", backref=backref("user"))
 
 class PasswordCheck(Base):
-    ___tablename__ = "password_checks"
+    __tablename__ = "password_checks"
     id = Column(Integer(), primary_key=True)
     hash_prefix = Column(String(), nullable=False)
     checkedAt = Column(DateTime(), server_default=func.now())
@@ -23,31 +23,31 @@ class PasswordCheck(Base):
 
     user_id = Column(Integer(), ForeignKey('users.id'))
 
-# mail_breachs = Table(
-#     'mail_breachs',
-#     Base.metadata,
-#     Column('mail_id', ForeignKey("email_checks.id"), primary_key=True),
-#     Column('breach_id', ForeignKey("breachs.id"), primary_key=True),
-#     extend_existing=True,
-# )
+mail_breachs = Table(
+    'mail_breachs',
+    Base.metadata,
+    Column('mail_id', ForeignKey("email_checks.id"), primary_key=True),
+    Column('breach_id', ForeignKey("breachs.id"), primary_key=True),
+    extend_existing=True,
+)
 
 # For further improvements of the app To check email breachs
-# class EmailCheck(Base):
-#     __tablename__ = "email_checks"
-#     id = Column(Integer(), primary_key=True)
-#     email_checked = Column(String(), nullable=False)
-#     date_checked = Column(DateTime(), server_default=func.now())
-#     no_of_breaches = Column(Integer(), nullable=False)
+class EmailCheck(Base):
+    __tablename__ = "email_checks"
+    id = Column(Integer(), primary_key=True)
+    email_checked = Column(String(), nullable=False)
+    date_checked = Column(DateTime(), server_default=func.now())
+    no_of_breaches = Column(Integer(), nullable=False)
 
-#     user_id = Column(Integer(), ForeignKey('users.id'))
-#     breaches = relationship("Breach", secondary=mail_breachs, back_populates="breachs")
+    user_id = Column(Integer(), ForeignKey('users.id'))
+    breaches = relationship("Breach", secondary=mail_breachs, back_populates="breachs")
 
-# class Breach(Base):
-#     __tablename__ = "breachs"
-#     id = Column(Integer(), primary_key=True)
-#     name = Column(String(), nullable=False)
-#     domain = Column(String(), nullable=False)
-#     breachDate = Column(String(), nullable=False)
-#     exposedData = Column(String(), nullable=False)
+class Breach(Base):
+    __tablename__ = "breachs"
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), nullable=False)
+    domain = Column(String(), nullable=False)
+    breachDate = Column(String(), nullable=False)
+    exposedData = Column(String(), nullable=False)
 
-#     emailChecks = relationship("EmailCheck", secondary=mail_breachs, back_populates="email_checks") 
+    emailChecks = relationship("EmailCheck", secondary=mail_breachs, back_populates="email_checks") 
---
+++ lib/db/pawned.py
@@ -0,0 +1,38 @@
+from models import User, PasswordCheck
+import requests
+from requests.models import HTTPError
+import getpass, hashlib
+
+
+password = getpass.getpass("Input a password: ")
+password_hash = hashlib.sha1(password.encode('utf-8')).hexdigest()
+print(password_hash)
+try: 
+    pass_res = requests.get(f"https://api.pwnedpasswords.com/range/{password_hash[0:5]}")
+    
+    if pass_res.status_code != 200:
+        raise HTTPError
+    
+except HTTPError:
+    print("\nError while attempting to connect to...")
+    print(f"{pass_res.url}")
+    print(f"HTTP Response code: {pass_res.status_code}")
+except Exception as e:
+    print("Error occurred checking for compromised password...")
+    print(f"{e}")
+else:
+    pass_matches = pass_res.text.splitlines()
+    pass_count = 0
+    
+    for hash in pass_matches:
+        pre, suf = hash.split(":")
+        if password_hash.upper() == password_hash[0:5].upper()+pre:
+            print(password_hash, "=", password_hash[0:5]+pre )
+            pass_count = int(suf)
+            print(pass_count)
+
+    if pass_count:
+        print("Kindly Ensure you change passwords of accounts using this password.")
+        print(f"Password has appeared {pass_count:,}in the database")
+    else:
+        print("Phewks. You're safe for now. Remember to keep good cyber Hygiene and Password keeping practices.")
